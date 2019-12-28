d32dbb937076bd6158a1df28d52d9444bf5d1c04
DongHaijun <donghaijun@jd.com>
2019-12-28 13:12:12 +0800

    build

--- pages/src/utils/request.js
+++ pages/src/utils/request.js
@@ -1,4 +1,4 @@
-const baseUrl = 'http://localhost:3000'
+const baseUrl = ''
 export default function request(url, requestConfig){
   url = baseUrl + url
   return fetch(url, requestConfig).then(res=>res.json())
