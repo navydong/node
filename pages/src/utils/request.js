f9c855c3b19f60ee8dcae81c478111de5c7d8291
DongHaijun <donghaijun@jd.com>
2019-12-28 22:27:01 +0800

    build

--- pages/src/utils/request.js
+++ pages/src/utils/request.js
@@ -1,4 +1,4 @@
-const baseUrl = 'http://localhost:3000'
+const baseUrl = process.env.NODE_ENV  === 'production'? '': 'http://localhost:3000'
 export default function request(url, config) {
   url = baseUrl + url
   const requestConfig = Object.assign({
