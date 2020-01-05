7eb1a9f100df8a68887bd69fa806c760ec18c159
DongHaijun <donghaijun@jd.com>
2020-01-06 00:30:11 +0800

    播放路径修改

--- pages/src/pages/movie.js
+++ pages/src/pages/movie.js
@@ -1,11 +1,11 @@
 
 import styles from './movie.css';
-
+const baseUrl = process.env.NODE_ENV  === 'production'? '': 'http://localhost:3000'
 export default function () {
   return (
     <div className={styles.normal}>
       <video controls className={styles.video}>
-        <source src="http://localhost:3000/movie/movie.mp4" type="video/mp4" />
+        <source src={`${baseUrl}/movie/movie.mp4`} type="video/mp4" />
         Your browser does not support the
         <code>video</code> element.
     </video>
